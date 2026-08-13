66818aaddf80de65df8fae171e416ae06b098a0b
Rohan Sharma <irohansh235@gmail.com>
2026-08-13 19:26:31 +0530

    docs: resume update

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -8,7 +8,7 @@ export const site = {
   email: "irohansh235@gmail.com",
   url: "https://irohansh.tech",
   resume:
-    "https://drive.google.com/file/d/1cxmV_goF6LY6Our4pWXi1necumS1Nm8B/view?usp=sharing",
+    "https://drive.google.com/file/d/1-opQ7VQfPZOpPqGYwsmgekE9Pxjz_QAZ/view?usp=sharing",
   socials: {
     github: "https://github.com/irohansh",
     linkedin: "https://www.linkedin.com/in/rohansharma89",
